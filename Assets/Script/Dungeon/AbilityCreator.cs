71276e02be28413665c15ec23a764362f5fceb36
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2023-07-06 05:09:21 +0900

    AbilityCreator
    능력 생성 기능 분리.

---
+++ Assets/Script/Dungeon/AbilityCreator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 보상 중 능력 생성 클래스. 능력(정령) 종류에 따른 목록들이 별도 존재하기에 필요.
+public class AbilityCreator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AbilityCrate()
+    {
+
+    }
+}
--- Assets/Script/Dungeon/RewardCreator.cs
+++ Assets/Script/Dungeon/RewardCreator.cs
@@ -10,12 +10,18 @@ public class RewardCreator : MonoBehaviour
     public GameObject CreateReward()
     {
         int index = Random.Range(0, rewardprefs.Count); // 무작위 난수 생성
-        Debug.Log("reward index : " + index);
+        //Debug.Log("reward index : " + index);
+        if (index == 0) // 0은 능력 
+        {
+            // 능력 생성 함수 실행.
+            this.gameObject.GetComponent<AbilityCreator>().AbilityCrate();
+        }
         GameObject reward = rewardprefs[index];
         //rewardprefs.Remove(rewardprefs[index]);
         return reward;
     }
 
+    // 오버로딩 함수. 시작 스테이지에서 
     public GameObject CreateReward(int max)
     {
         int index = Random.Range(0, max); // 무작위 난수 생성
