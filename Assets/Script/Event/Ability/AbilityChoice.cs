7b68d69fab0fdc0f58374dc47b194fb78c17082a
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2023-11-15 14:04:27 +0900

    Ability list_2(error fix)
    버튼에 add listener가 진행되지 않는 오류 해결.
    버튼 활성화 전에 add listener를 수행해서 getcomponent 전에 함수 실행되는 것이 원인이었음.

--- Assets/Script/Event/Ability/AbilityChoice.cs
+++ Assets/Script/Event/Ability/AbilityChoice.cs
@@ -34,6 +34,9 @@ public class AbilityChoice : MonoBehaviour
     {
         // 활성화 시, 오브젝트에 부착된 버튼 컴포넌트 저장.
         button = this.gameObject.GetComponent<Button>();
+        Debug.Log(button.name);
+        // 버튼 onClick() 이벤트에 리스너 추가. 클릭 시 해당 능력 정보를 전달.
+        button.onClick.AddListener(() => aLManager.GetSelected(this.gameObject));
     }
 
     public void SetChoiceValue(int[] indexArray)
@@ -42,13 +45,8 @@ public class AbilityChoice : MonoBehaviour
         typeIndex = tmpArray[0];
         abilityID = tmpArray[1];
         plusValue = tmpArray[2];
-        Debug.Log(typeIndex + "," + abilityID + "," + plusValue);
-        if (aLManager != null)
-        {
-            Debug.Log("있음");
-        }
-        // 버튼 onClick() 이벤트에 리스너 추가. 클릭 시 해당 능력 정보를 전달.
-        button.onClick.AddListener(() => aLManager.AddAbility(typeIndex, abilityID, plusValue));
+
+
     }
 
     public void ChoiceClicked()
--- Assets/Script/Event/Ability/AbilityListManager.cs
+++ Assets/Script/Event/Ability/AbilityListManager.cs
@@ -12,6 +12,11 @@ public class AbilityListManager : MonoBehaviour
     [SerializeField]
     GameObject abilityUIPref;
 
+    // 임시 저장 변수들
+    int abilityIndex;
+    int id;
+    int value;
+
     void Start()
     {
         
@@ -24,14 +29,34 @@ public class AbilityListManager : MonoBehaviour
 
     // 현재 보유 리스트에 선택된 능력을 추가하는 함수.
     // 능력 선택 이벤트에서 버튼 클릭으로 인한 OnClick() 이벤트 발생 시 실행되는 이벤트 핸들러.
-    public void AddAbility(int _abilityIndex, int _id, int _value) // 순서대로 능력 인덱스, 능력 DB 상 id, 가산 수치(후에 레벨로 변경가능)
+    private void AddAbility() // 순서대로 능력 인덱스, 능력 DB 상 id, 가산 수치(후에 레벨로 변경가능)
+    {
+        Ability ability = new Ability();
+        
+    }
+
+    public void GetSelected(GameObject _selected)
+    {
+        Debug.Log(_selected.name + "전달");
+    }
+
+    public void GetAbilityIndex(int _abilityIndex)
     {
         Debug.Log("능력 선택.");
         Debug.Log("능력 인덱스 : " + _abilityIndex);
-        Debug.Log("능력 ID : " + _id);
-        Debug.Log("가산 수치 : " + _value);
-        Ability ability = new Ability();
+        abilityIndex = _abilityIndex;
+    }
 
+    public void GetId(int _id)
+    {
+        Debug.Log("능력 ID : " + _id);
+        id = _id;
+    }
+
+    public void GetValue(int _value)
+    {
+        Debug.Log("가산 수치 : " + _value);
+        value = _value;
 
     }
 }
