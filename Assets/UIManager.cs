53d8ec398a99dd768376d791515defada036e244
SKYlight Studio <86962788+SkylightStudio07@users.noreply.github.com>
2024-03-09 23:15:34 +0900

    Stage UI Manager Alpha v1
    스테이지가 Scene이 아닌 프리팹으로 바뀌는 관계로 새로운 방법이 필요. 현재는 UIManager.cs가 Scene을 기준으로 변경됨.

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     public RectTransform uiElement;
+    [SerializeField] private TextMeshProUGUI uiText;
     public float moveDuration = 1.5f;
     private float uIDisableTime = 5.0f;
 
@@ -13,6 +16,17 @@ public class UIManager : MonoBehaviour
     {
         // 초기 위치를 화면 상단으로 설정
         uiElement.anchoredPosition = new Vector2(0, Screen.height);
+        uiText = transform.GetComponentInChildren<TextMeshProUGUI>();
+
+        // 찾은 TextMeshProUGUI에 접근하여 수정 또는 사용
+        if (uiText != null)
+        {
+            SetStageUIText();
+        }
+        else
+        {
+            Debug.LogError("TextMeshProUGUI를 확인할 없습니다.");
+        }
 
         // LeanTween을 사용하여 UI 요소를 아래로 이동
         LeanTween.move(uiElement, new Vector2(0, -50), moveDuration).setEase(LeanTweenType.easeOutExpo);
@@ -24,4 +38,20 @@ public class UIManager : MonoBehaviour
         yield return new WaitForSeconds(uIDisableTime);
         LeanTween.move(uiElement, new Vector2(0, Screen.height), moveDuration).setEase(LeanTweenType.easeOutExpo);
     }
-}
+
+    void SetStageUIText()
+    {
+
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        if(currentSceneName == "HomeScene")
+        {
+            uiText.text = "봄이 닿지 않는 정원";
+        }
+        else if (currentSceneName == "DungeonScene_JSTest")
+        {
+            uiText.text = "깊고 어두운 숲 초입";
+        }
+        Debug.Log("현재 Scene 이름 : " + currentSceneName + " / Stage UI Text 출력 확인");
+
+    }
+    }
