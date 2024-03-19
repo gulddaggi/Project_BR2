cfa3c5b037ba69940122a5ec6c3a8cdd866fe356
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2024-03-20 03:05:36 +0900

    Add SaveData class
    저장 데이터 클래스 추가
    - 저장 대상 데이터 변수 추가
    - 생성자에서 해당 변수 저장 구현. 해당 클래스의 객체 생성(인스턴스화) 시 저장 대상 데이터 매개변수로 전달 필요.

---
+++ Assets/Script/Save&Load/SaveData.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 저장 데이터 클래스. 인스턴스화 시 저장 데이터 변수를 매개변수로 받아 멤버 변수에 대입
+[System.Serializable]
+public class SaveData
+{
+    // 저장 데이터 변수
+    // 영구 업그레이드 레벨
+    [SerializeField]
+    private List<int> upgradeLevelList;
+    // 각 NPC별 회차당 첫 조우 횟수
+    [SerializeField]
+    private List<int> NPCEncounterCount;
+    // 재화 잼 보유량
+    [SerializeField]
+    private int gem;
+    // 던전 진입 시도 횟수
+    [SerializeField]
+    private int dungeonEntryCount;
+    // 보스 처치 횟수
+    [SerializeField]
+    private int bossKilledCount;
+
+    //클래스 생성자
+    public SaveData(List<int> _upgradeLevelList, List<int> _NPCEncounterCount, int _gem, int _dungeonEntryCount, int _bossKilledCount)
+    {
+        upgradeLevelList = new List<int>();
+        foreach (int level in _upgradeLevelList)
+        {
+            upgradeLevelList.Add(level);
+        }
+
+        NPCEncounterCount = new List<int>();
+        foreach (int count in _NPCEncounterCount)
+        {
+            NPCEncounterCount.Add(count);
+        }
+
+        _gem = gem;
+
+        dungeonEntryCount = _dungeonEntryCount;
+
+        bossKilledCount = _bossKilledCount;
+    }
+
+}
