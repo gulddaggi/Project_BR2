989ed6b2092dfe922dd0f0af18e9c8038196bcd4
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2024-03-21 18:55:57 +0900

    Load savefile & Convert to SaveData class
    세이브파일 로드 및 로드 데이터 SaveData 클래스 전환.

--- Assets/Script/Save&Load/SaveNLoad.cs
+++ Assets/Script/Save&Load/SaveNLoad.cs
@@ -5,6 +5,7 @@ using System.IO;
 
 public class SaveNLoad : MonoBehaviour
 {
+    // 저장 수행
     public static void Save()
     {
         // 매개변수 구성
@@ -42,7 +43,7 @@ public class SaveNLoad : MonoBehaviour
     // SaveData 클래스를 JSON으로 변환
     static string SaveDataToJSON(SaveData _saveData)
     {
-        string jsonData = JsonUtility.ToJson(_saveData);
+        string jsonData = JsonUtility.ToJson(_saveData, true);
         return jsonData;
     }
 
@@ -55,12 +56,47 @@ public class SaveNLoad : MonoBehaviour
 
             saveFile.Write(bytes, 0, bytes.Length);
         }
+
+        Debug.Log("세이브 파일 생성 완료.");
+        Debug.Log("경로 : " + GetPath());
     }
 
+    // 저장 경로 반환
     static string GetPath()
     {
-        return Path.Combine(Application.persistentDataPath, "save.abcd");
+        return Path.Combine(Application.persistentDataPath, "save.json");
     }
 
+    // 불러오기 수행
+    public static void Load()
+    {
+        if (!File.Exists(GetPath()))
+        {
+            Debug.Log("세이브 파일 없음.");
+            return;
+        }
+
+        string jsonData = LoadSaveFile();
+        SaveData saveData = JSONTOSaveData(jsonData);
+    }
+
+    static string LoadSaveFile()
+    {
+        using (FileStream saveFile = new FileStream(GetPath(), FileMode.Open, FileAccess.Read))
+        {
+            byte[] bytes = new byte[(int)saveFile.Length];
+
+            saveFile.Read(bytes, 0, (int)saveFile.Length);
+
+            string jsonData = System.Text.Encoding.UTF8.GetString(bytes);
+            return jsonData;
+        }
+    }
+
+    static SaveData JSONTOSaveData(string _saveData)
+    {
+        SaveData saveData = JsonUtility.FromJson<SaveData>(_saveData);
+        return saveData;
+    }
 
 }
