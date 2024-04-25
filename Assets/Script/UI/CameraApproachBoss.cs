f1013f59712eff9517c939d244cc4b88aac6140b
SKYlight Studio <jamespk0725@gmail.com>
2024-04-26 05:27:33 +0900

    Harpy-Player Moving Issue
    컷신 시간에 플레이어가 움직일 수 있었던 버그가 수정됨.

--- Assets/Script/UI/CameraApproachBoss.cs
+++ Assets/Script/UI/CameraApproachBoss.cs
@@ -6,11 +6,14 @@ public class CameraApproachBoss : MonoBehaviour
     public float approachSpeed = 1.0f;
 
     private bool isApproaching = false;
+    GameObject Player;
 
     void Start()
     {
         // 스테이지 변경 후 1.5초 후에 CameraApproach 함수 호출
         Invoke("StartCameraApproach", 2.5f);
+        Player = GameObject.FindGameObjectWithTag("Player");
+        DisablePlayerMovement(false);
     }
 
     void StartCameraApproach()
@@ -28,6 +31,7 @@ public class CameraApproachBoss : MonoBehaviour
             // 카메라가 목표 지점에 도달했는지 확인
             if (Vector3.Distance(transform.position, bossTransform.position) < 0.1f)
             {
+                DisablePlayerMovement(true);
                 isApproaching = false;
                 DisableCamera();
             }
@@ -37,5 +41,22 @@ public class CameraApproachBoss : MonoBehaviour
     void DisableCamera()
     {
         gameObject.SetActive(false); // 카메라를 비활성화
+    }
+
+    void DisablePlayerMovement(bool CutSceneOver)
+    {
+        PlayerController playerController = Player.GetComponent<PlayerController>();
+
+        if ( playerController)
+        {
+            if (CutSceneOver == false)
+            {
+                playerController.CanMove = false;
+            }
+            else
+            {
+                playerController.CanMove = true;
+            }
+        }
     }
 }
