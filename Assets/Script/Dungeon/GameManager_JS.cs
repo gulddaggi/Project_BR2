e2e8f57f15b237d019f02b64f8ecac31c6cb875b
Zel-da <anjongman1029@gmail.com>
2024-05-10 17:27:01 +0900

    Audio
    - 보스 오디오 추가
    - 튜토리얼 씬 넘어가도록 변수 추가

--- Assets/Script/Dungeon/GameManager_JS.cs
+++ Assets/Script/Dungeon/GameManager_JS.cs
@@ -120,6 +120,9 @@ public class GameManager_JS : MonoBehaviour
     private bool isLoad = false;
 
     public bool isCutScene = false;
+    
+    // 튜토리얼 재생을 위한 변수
+    private bool Tutorial = false;
 
     private void Awake()
     {
@@ -203,15 +206,31 @@ public class GameManager_JS : MonoBehaviour
 
     public void InitStage()
     {
-        StartCoroutine(FadeInPanel());
-        stageQueue.Clear();
-        dungeonCount = 0;
-        SceneManager.LoadScene("HomeScene");
-        Coin = 0;
-        coinText_Play.transform.gameObject.SetActive(false);
-        isMoveOn = true;
-        isEventOn = false;
-        ResetEncounter();
+        if(Tutorial == true)
+        {
+            StartCoroutine(FadeInPanel());
+            stageQueue.Clear();
+            dungeonCount = 0;
+            SceneManager.LoadScene("HomeScene");
+            Coin = 0;
+            coinText_Play.transform.gameObject.SetActive(false);
+            isMoveOn = true;
+            isEventOn = false;
+            ResetEncounter();
+        }
+        else
+        {
+            Tutorial = true;
+            StartCoroutine(FadeInPanel());
+            stageQueue.Clear();
+            dungeonCount = 0;
+            SceneManager.LoadScene("Tutorial");
+            Coin = 0;
+            coinText_Play.transform.gameObject.SetActive(false);
+            isMoveOn = true;
+            isEventOn = false;
+            ResetEncounter();
+        }
     }
 
     void ResetEncounter()
