615167609347c3c1533f2dfa05dec41eeec079d4
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2024-05-12 00:50:28 +0900

    Edit &Apply new stages
    새로 추가된 스테이지 수정 및 적용
    + 적 스폰 클래스 리팩토링

--- Assets/Script/Enemy/EnemySpawner.cs
+++ Assets/Script/Enemy/EnemySpawner.cs
@@ -20,87 +20,48 @@ public class EnemySpawner : MonoBehaviour
     int curWaveCount = 0;
 
     // 최대 웨이브
+    [SerializeField]
     int maxWaveCount = 2;
 
     // 현재 소환된 적 수
     public int curEnemyCount = 0;
 
-    // 웨이브 당 소환할 지점의 수. 에디터 상에서 직접 지정.
-    // 최대 수 초과하지 않도록 주의.
-    [SerializeField]
-    int[] spawnCount = new int[2];
-
-    // 스폰 제어 트리거
-    bool spawnTrigger = false;
-
-    public bool SPAWNTRIGGER { get { return spawnTrigger; } }
-
     void Start()
     {
-        // 에러 감지.
-        if (spawnCount[0] + spawnCount[1] > spawnPoints.Count)
-        {
-            Debug.LogError("지정한 소환 지점의 수 초과! EnemySpanwer 확인");
-        }
-
-        if (spawnPoints.Count != 0)
+        if (spawnPoints.Count != 0 && !IsAllWaveEnd())
         {
             EnemySpawn();
-            this.gameObject.GetComponentInParent<Dungeon>().SetEnemyCount(curEnemyCount);
         }
-
-    }
-
-    void Update()
-    {
-        if (spawnTrigger && curEnemyCount <= 0)
+        else
         {
-            spawnTrigger = false;
-            if (!IsAllWaveEnd())
-            {
-                EnemySpawn();
-            }
+            this.gameObject.GetComponentInParent<Dungeon>().SetEnemyCount(curEnemyCount);
         }
     }
 
     // 적 스폰
     void EnemySpawn()
     {
-        int minSpawnPoints = 3; // 최소 spawnPoint 수
-        int maxSpawnPoints = 6; // 최대 spawnPoint 수
+        int minSpawnPoints = 2; // 최소 spawnPoint 수
+        int maxSpawnPoints = 5; // 최대 spawnPoint 수
         int numSpawnPoints = Random.Range(minSpawnPoints, maxSpawnPoints);
-        List<int> spawnCounts = new List<int>();
+
+        List<int> spawnPointsList = new List<int>();
         for (int i = 0; i < numSpawnPoints; i++)
         {
             int randomSpawnCount = Random.Range(0, spawnPoints.Count);
-            spawnCounts.Add(randomSpawnCount);
+            spawnPointsList.Add(randomSpawnCount);
         }
-        float rad = 2f;
-        float angle;
+
         EnemyChoice();
 
-        if (curWaveCount == 0)
+        for (int i = 0; i < numSpawnPoints; i++)
         {
-            for (int i = 0; i < numSpawnPoints; i++)
-            {
-                GameObject spawnEnemy = Instantiate(curEnemy, 
-                    spawnPoints[spawnCounts[i]].position ,
-                     Quaternion.identity);
-                spawnEnemy.transform.SetParent(this.gameObject.transform);
-                ++curEnemyCount;
-            }
-            ++curWaveCount;
+            GameObject spawnEnemy = Instantiate(curEnemy, spawnPoints[spawnPointsList[i]].position, Quaternion.identity);
+            spawnEnemy.transform.SetParent(this.gameObject.transform);
+            ++curEnemyCount;
         }
-        else
-        {
-                for (int i = 0; i < numSpawnPoints; i++)
-                {
-                    GameObject spawnEnemy = Instantiate(curEnemy, spawnPoints[spawnCounts[i]].position, Quaternion.identity);
-                    spawnEnemy.transform.SetParent(this.gameObject.transform);
-                    ++curEnemyCount;
-                }
-                ++curWaveCount;
-        }
+        ++curWaveCount;
+
         this.gameObject.GetComponentInParent<Dungeon>().SetEnemyCount(curEnemyCount);
     }
 
@@ -109,15 +70,15 @@ public class EnemySpawner : MonoBehaviour
         --curEnemyCount;
         this.gameObject.GetComponentInParent<Dungeon>().DecEnemyCount();
 
-        if (curEnemyCount == 0 && curWaveCount != 0)
+        if (curEnemyCount == 0 && !IsAllWaveEnd())
         {
-            spawnTrigger = true;
+            EnemySpawn();
         }
     }
 
     public bool IsAllWaveEnd()
     {
-        return (curWaveCount > maxWaveCount);
+        return (curWaveCount >= maxWaveCount);
     }
 
     // 소환할 적 추첨
